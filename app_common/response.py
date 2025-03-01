7c8f7114657f85a3afabf1c143c91568a7402a8d
reinhart <reinhartsoplantila@gmail.com>
2025-03-01 23:40:58 +0800

    feat: fix reponse method for grade api

---
+++ app_common/response.py
@@ -0,0 +1,18 @@
+from rest_framework.response import Response
+from rest_framework import status
+
+def success_response(serializer, message):
+    return Response({
+        "status_code":status.HTTP_200_OK,
+        "status":"success",
+        "message":message,
+        "data":serializer.data
+    }, status=status.HTTP_200_OK)
+
+def delete_reponse(message):
+    return Response({
+        "status_code":status.HTTP_204_NO_CONTENT,
+        "status":"success",
+        "message":"success delete data",
+        "data":None
+    }, status=status.HTTP_200_OK)
--- app_common/views.py
+++ app_common/views.py
@@ -5,61 +5,61 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import Grade, Term, Status
 from .serializers import GradeSerializer, TermSerializers, StatusSerializers
+from .response import success_response, delete_reponse
 
 # Create your views here.
 class GradeApiView(APIView):
     def get(self, request, grade_id=None):
         grade_obj=Grade.objects.filter(id=grade_id)
         serializer=GradeSerializer(grade_obj, many=True)
-        return Response({
-            "status_code":status.HTTP_200_OK,
-            "status":"success",
-            "message":"success retrieved data",
-            "data":serializer.data
-        }, status=status.HTTP_200_OK)
+        return success_response(serializer, message='success retrieved data')
         
     def post(self, request):
         serializer=GradeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({
-                "status_code":status.HTTP_200_OK,
-                "status":"success",
-                "message":"success insert data",
-                "data":serializer.data
-            }, status=status.HTTP_200_OK)
+            return success_response(serializer, message='success insert data')
+            # return Response({
+            #     "status_code":status.HTTP_200_OK,
+            #     "status":"success",
+            #     "message":"success insert data",
+            #     "data":serializer.data
+            # }, status=status.HTTP_200_OK)
 
     def put(self, request, grade_id):
         grade_obj=get_object_or_404(Grade, id=grade_id)
         serializer=GradeSerializer(grade_obj, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({
-                "status_code":status.HTTP_200_OK,
-                "status":"success",
-                "message":"success update data",
-                "data":serializer.data
-            }, status=status.HTTP_200_OK)
+            return success_response(serializer, message='success update data')
+            # return Response({
+            #     "status_code":status.HTTP_200_OK,
+            #     "status":"success",
+            #     "message":"success update data",
+            #     "data":serializer.data
+            # }, status=status.HTTP_200_OK)
     
     def patch(self, request, grade_id):
         grade_obj=get_object_or_404(Grade, id=grade_id)
         serializer=GradeSerializer(grade_obj, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({
-                "status_code":status.HTTP_200_OK,
-                "status":"success",
-                "message":"success update data",
-                "data":serializer.data
-            }, status=status.HTTP_200_OK)
+            return success_response(serializer, message='success update data')
+            # return Response({
+            #     "status_code":status.HTTP_200_OK,
+            #     "status":"success",
+            #     "message":"success update data",
+            #     "data":serializer.data
+            # }, status=status.HTTP_200_OK)
     
     def delete(self, request, grade_id):
         grade_obj=get_object_or_404(Grade, id=grade_id)
         grade_obj.delete()
-        return Response({
-            "status_code":status.HTTP_200_OK,
-            "status":"success",
-            "message":"success delete data",
-        }, status=status.HTTP_200_OK)
-        
+        return delete_reponse(message='success delete data')
+        # return Response({
+        #     "status_code":status.HTTP_200_OK,
+        #     "status":"success",
+        #     "message":"success delete data",
+        # }, status=status.HTTP_200_OK)
+
         
