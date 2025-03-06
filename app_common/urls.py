5b0fd050d6a8295e3e4baf1c6239784869798fcf
reinhart <reinhartsoplantila@gmail.com>
2025-03-06 13:36:41 +0800

    feat: add greade, term, status API endpoint & app_common url

--- app_common/urls.py
+++ app_common/urls.py
@@ -3,5 +3,12 @@ from .views import GradeApiView, TermApiView, StatusApiView
 
 app_name='app_common'
 urlpatterns=[
-
+    path('grade/', GradeApiView.as_view(), name="all grade"),
+    path('grade/<int:grade_id>', GradeApiView.as_view(), name='specific grade'),
+    
+    path('term/', TermApiView.as_view(), name="all term"),
+    path('term/<int:term_id>', TermApiView.as_view(), name="specific term"),
+    
+    path('status/', StatusApiView.as_view(), name="all status"),
+    path('status/<int:status_id>', StatusApiView.as_view(), name="specific status"),
 ]
