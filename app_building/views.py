1dcbd5d71b87f07efe57135ed731842ddf901481
reinhart <reinhartsoplantila@gmail.com>
2025-03-12 18:30:20 +0800

    feat: add room, faculty, and department method

--- app_building/views.py
+++ app_building/views.py
@@ -45,3 +45,127 @@ class BuildingApiView(APIView):
     
     def options(self, request, *args, **kwargs):
         return super().options(request, *args, **kwargs)
+    
+class RoomApiView(APIView):
+    def get(self, request, room_id=None):
+        if room_id is not None:
+            room_obj=get_object_or_404(Room, id=room_id)
+            serializer=RoomSerializer(room_obj)
+        else:
+            room_obj=Room.objects.all()
+            serializer=RoomSerializer(room_obj, many=True)
+        return success_response(serializer.data, message='success retrieved data')
+    
+    def post(self, request):
+        serializer=RoomSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return success_response(serializer.data, message='success create data')
+        return error_400_response(serializer)
+
+    def put(self, request, room_id):
+        room_obj=get_object_or_404(Room, id=room_id)
+        serializer=RoomSerializer(room_obj, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return success_response(serializer.data, message='success update data')
+        return error_400_response(serializer)
+        
+    def patch(self, request, room_id):
+        room_obj=get_object_or_404(Room, id=room_id)
+        serializer=RoomSerializer(room_obj, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return success_response(serializer.data, message="success update data")
+        return error_400_response(serializer)
+
+    def delete(self, request, room_id):
+        room_obj=get_object_or_404(Room, id=room_id)
+        room_obj.delete()
+        return delete_reponse(message='success delete data')
+    
+    def options(self, request, *args, **kwargs):
+        return super().options(request, *args, **kwargs)
+    
+class FacultyApiView(APIView):
+    def get(self, request, faculty_id=None):
+        if faculty_id is not None:
+            faculty_obj=get_object_or_404(Faculty, id=faculty_id)
+            serializer=FacultySerializer(faculty_obj)
+        else:
+            faculty_obj=Faculty.objects.all()
+            serializer=FacultySerializer(faculty_obj, many=True)
+        return success_response(serializer.data, message='success retrieved data')
+    
+    def post(self, request):
+        serializer=FacultySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return success_response(serializer.data, message='success create data')
+        return error_400_response(serializer)
+
+    def put(self, request, faculty_id):
+        faculty_obj=get_object_or_404(Faculty, id=faculty_id)
+        serializer=FacultySerializer(faculty_obj, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return success_response(serializer.data, message='success update data')
+        return error_400_response(serializer)
+        
+    def patch(self, request, faculty_id):
+        faculty_obj=get_object_or_404(Faculty, id=faculty_id)
+        serializer=FacultySerializer(faculty_obj, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return success_response(serializer.data, message="success update data")
+        return error_400_response(serializer)
+
+    def delete(self, request, faculty_id):
+        faculty_obj=get_object_or_404(Faculty, id=faculty_id)
+        faculty_obj.delete()
+        return delete_reponse(message='success delete data')
+    
+    def options(self, request, *args, **kwargs):
+        return super().options(request, *args, **kwargs)
+    
+class DepartmentApiView(APIView):
+    def get(self, request, department_id=None):
+        if department_id is not None:
+            department_obj=get_object_or_404(Department, id=department_id)
+            serializer=DepartmentSerializer(department_obj)
+        else:
+            department_obj=Department.objects.all()
+            serializer=DepartmentSerializer(department_obj, many=True)
+        return success_response(serializer.data, message='success retrieved data')
+    
+    def post(self, request):
+        serializer=DepartmentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return success_response(serializer.data, message='success create data')
+        return error_400_response(serializer)
+
+    def put(self, request, department_id):
+        department_obj=get_object_or_404(Department, id=department_id)
+        serializer=DepartmentSerializer(department_obj, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return success_response(serializer.data, message='success update data')
+        return error_400_response(serializer)
+        
+    def patch(self, request, department_id):
+        department_obj=get_object_or_404(Department, id=department_id)
+        serializer=DepartmentSerializer(department_obj, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return success_response(serializer.data, message="success update data")
+        return error_400_response(serializer)
+
+    def delete(self, request, department_id):
+        department_obj=get_object_or_404(Department, id=department_id)
+        department_obj.delete()
+        return delete_reponse(message='success delete data')
+    
+    def options(self, request, *args, **kwargs):
+        return super().options(request, *args, **kwargs)
+    
