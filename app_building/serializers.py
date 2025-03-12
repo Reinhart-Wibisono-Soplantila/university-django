74aedb64ad57a80d1abaffee1bc619c7396f7905
reinhart <reinhartsoplantila@gmail.com>
2025-03-12 20:10:52 +0800

    feat: add department code generator

--- app_building/serializers.py
+++ app_building/serializers.py
@@ -20,3 +20,16 @@ class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model=Department
         fields='__all__'
+    
+    def create(self, validated_data):
+        faculty=validated_data['faculty']
+        faculty_code=faculty.faculty_code
+        last_department=Department.objects.filter(faculty=faculty).order_by('-department_code').first()
+        if last_department is not None:
+            las_number=int(last_department.department_code[1:])
+            new_number=las_number+1
+        else:
+            new_number=1
+        department_code=f"{faculty_code}{new_number:03d}"
+        validated_data['department_code']=department_code
+        return super().create(validated_data)
