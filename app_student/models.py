eae0780d709c59df5e9761f89986f57014f17622
reinhart <reinhartsoplantila@gmail.com>
2025-03-26 19:49:04 +0800

    feat: split stduent class

--- app_student/models.py
+++ app_student/models.py
@@ -6,18 +6,9 @@ from django.utils.timezone import now
 # Create your models here.
 class Student(models.Model):
     nim=models.CharField(max_length=15, db_index=True, unique=True, editable=False)
-    fullname=models.CharField(max_length=255)
     faculty=models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name="student_faculty")
     department=models.ForeignKey(Department, on_delete=models.CASCADE, related_name="student_department")
     registration_year = models.IntegerField(default=now().year)
-    phone_number=PhoneNumberField()
-    address=models.TextField()
-    city_birth=models.CharField(max_length=255)
-    date_birth=models.DateField()
-    email=models.EmailField()
-    status=models.ForeignKey(Status, on_delete=models.CASCADE, default=1)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
     
     def save(self, *args, **kwargs):
         if not self.nim:
@@ -31,4 +22,16 @@ class Student(models.Model):
                 student_number=1
             self.nim=f'{faculty_code}{department_code:02d}1{year}1{student_number:03d}'
         super().save(*args, **kwargs)
+
+class StudentProfile(models.Model):
+    fullname=models.CharField(max_length=255)
+    phone_number=PhoneNumberField()
+    address=models.TextField()
+    city_birth=models.CharField(max_length=255)
+    date_birth=models.DateField()
+    email=models.EmailField()
+    status=models.ForeignKey(Status, on_delete=models.CASCADE, default=1)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
         
--- app_student/serializers.py
+++ app_student/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Student
+from app_common.models import Department
 from phonenumber_field.serializerfields import PhoneNumberField
+from rest_framework.exceptions import ValidationError
 
 class StudentSerializer(serializers.ModelSerializer):
     phone_number=PhoneNumberField()
@@ -8,6 +10,13 @@ class StudentSerializer(serializers.ModelSerializer):
         model=Student
         fields='__all__'
         read_only_fields=['nim']
+    
+    def validate(self, data):
+        faculty = data.get("faculty") or self.instance.faculty
+        department = data.get("department") or self.instance.department
+        if "faculty" in data and department and not faculty.departments.filter(id=department.id).exists():
+            raise ValidationError({"detail": "Department is not listed in this Faculty"})
+        return data
     
     def update(self, instance, validated_data):
         faculty=validated_data.get('faculty', instance.faculty)
@@ -18,11 +27,11 @@ class StudentSerializer(serializers.ModelSerializer):
         department_code=department.id if department!=instance.department else instance.department.id
         year=str(registration_year)[-2:] if registration_year!=instance.registration_year else str(instance.registration_year)[-2:]
         
-        if registration_year!=instance.registration_year or department!=instance.department:
-            last_student=Student.objects.filter(registration_year=registration_year, department=department).order_by('-nim').first()
+        if faculty_code!=instance.faculty.faculty_code or registration_year!=instance.registration_year or department_code!=instance.department.id:
+            last_student=Student.objects.filter(faculty=faculty, registration_year=registration_year, department=department).order_by('-nim').first()
             student_number=int(last_student.nim[-3:])+1 if last_student else 1
             instance.nim=f'{faculty_code}{department_code:02d}1{year}1{student_number:03d}'
-        
+                
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         
