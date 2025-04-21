945fe63e8422feca76ba48a48520feaa4d8eefc5
reinhart <reinhartsoplantila@gmail.com>
2025-04-22 01:16:28 +0800

    feat: add update for superadmin

--- app_staff/serializers.py
+++ app_staff/serializers.py
@@ -4,11 +4,11 @@ from phonenumber_field.serializerfields import PhoneNumberField
 from .models import TeachingStaff, PositionTeachingStaff, AdministrativeStaff, AreaOfExpertise, SuperAdminStaff
 from rest_framework.exceptions import ValidationError
 
-class UserSerializer_Regis(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User  # Model User
         fields = ['email', 'username', 'password', 'first_name', 'last_name']
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {**{field: {'required': True} for field in fields}, 'password': {'write_only': True}}
     
     def update(self, instance, validated_data):
         password = validated_data.pop('password', None)
@@ -30,8 +30,8 @@ class SuperAdminSerializer_Get(serializers.ModelSerializer):
 
 class SuperAdminSerializer_Regis(serializers.ModelSerializer):
     class Meta:
-        model = SuperAdminStaff
-        fields = ['nip', 'full_name', 'phone_number']
+        model=SuperAdminStaff
+        fields=['nip', 'full_name', 'phone_number']
 
     def validate_nip(self, value):
         if SuperAdminStaff.objects.filter(nip=value).exists():
@@ -44,11 +44,11 @@ class SuperAdminSerializer_Regis(serializers.ModelSerializer):
         return value
 
 class SuperAdminSerializer_Create(serializers.ModelSerializer):
-    user = UserSerializer_Regis()
+    user = UserSerializer()
     
     class Meta:
-        model = SuperAdminStaff
-        fields = ['nip', 'full_name', 'phone_number', 'user']
+        model=SuperAdminStaff
+        fields=['nip', 'full_name', 'phone_number', 'user']
 
     def validate_nip(self, value):
         if SuperAdminStaff.objects.filter(nip=value).exists():
@@ -61,45 +61,51 @@ class SuperAdminSerializer_Create(serializers.ModelSerializer):
         return value
     
     def create(self, validated_data):
-        user_data = validated_data.pop('user')
+        user_data=validated_data.pop('user')
         try:
             user=User.objects.create_user(**user_data)
             superadminstaff = SuperAdminStaff.objects.create(user=user, **validated_data)
         except Exception as e:
-            # Jika ada error, kita lemparkan ValidationError untuk memberitahukan kesalahan
             raise ValidationError(f"An error occurred while creating user: {str(e)}")
         return superadminstaff
-
+    
 class SuperAdminSerializer_Update(serializers.ModelSerializer):
-    user = UserSerializer_Regis()
-    superadminstaff = SuperAdminSerializer_Get()
-
+    user = UserSerializer()
+    
     class Meta:
-        model = SuperAdminStaff
-        fields = ['user', 'superadminstaff']
-        
+        model=SuperAdminStaff
+        fields=['nip', 'full_name', 'phone_number', 'user']
+
+    def validate_nip(self, value):
+        if SuperAdminStaff.objects.filter(nip=value).exists():
+            raise serializers.ValidationError("NIP must be unique.")
+        return value
+    
     def validate_phone_number(self, value):
-        # Hanya cek duplikat jika phone_number tidak kosong
-        if value and SuperAdminStaff.objects.filter(phone_number=value).exists():
+        if TeachingStaff.objects.filter(phone_number=value).exists():
             raise serializers.ValidationError("Phone number must be unique.")
         return value
-     
+    
     def update(self, instance, validated_data):
-        user_data = validated_data.pop('user')
-        superadminstaff_data = validated_data.pop('superadminstaff')
+        user_data=validated_data.pop('user', None)
         
-        superadmin_serializer = SuperAdminSerializer_Get(
-            instance=instance.superadminstaff, data=superadminstaff_data, partial=True)
-        superadmin_serializer.is_valid(raise_exception=True)
-        superadmin_serializer.save()
+        try:
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+            instance.save()
+            
+            if user_data:
+                user=instance.user
+                for attr, value in user_data.items():
+                    if attr=='password':
+                        user.set_password(value)
+                    else:
+                        setattr(user, attr, value)
+                user.save()
+        except Exception as e:
+            raise ValidationError(f"An error occurred while updating user: {str(e)}")
         
-        serializer=UserSerializer_Regis(instance=instance.user, data=user_data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return instance 
-
-
-
+        return instance
 
 class TeachingStaffSerializer(serializers.ModelSerializer):
     phone_number=PhoneNumberField()
