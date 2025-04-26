9320e11622e5f059cb68402d051695dcf1d35348
reinhart <reinhartsoplantila@gmail.com>
2025-04-26 16:06:10 +0800

    delete temp folder

--- app_student/urls.py
+++ app_student/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('<str:student_nim>', StudentAPIView.as_view(), name='get_specific'),
     # # path('profile/<str:student_nim>', StudentProfileApiView.as_view(), name='get_specific'),
     
-    path('faculty-autocomplete/', FacultyAutocomplete.as_view(), name='faculty-autocomplete'),
-    path('department-autocomplete/', DepartmentAutocomplete.as_view(), name='department-autocomplete'),
+    # path('faculty-autocomplete/', FacultyAutocomplete.as_view(), name='faculty-autocomplete'),
+    # path('department-autocomplete/', DepartmentAutocomplete.as_view(), name='department-autocomplete'),
 ]
