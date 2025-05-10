7434bbc64f670c338828935f8d0d6107ec7298f7
reinhart <reinhartsoplantila@gmail.com>
2025-05-11 00:04:45 +0800

    app_course: refactor courseserializer

--- app_course/serializers.py
+++ app_course/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import *
 from django.utils import timezone
 from app_common.serializers import AcademicProgramSerializer, FacultySerializer
+from django.shortcuts import get_object_or_404
 
 class CourseTypeSerializer(serializers.ModelSerializer):
     class Meta:
@@ -35,24 +36,53 @@ class CourseSerializer(serializers.ModelSerializer):
         fields='__all__'
         read_only_fields=['course_id']
     
+    def validate_academic_program_id(self, value):
+        if not AcademicProgram.objects.filter(id=value).exists():
+            raise serializers.ValidationError("Academic Program dengan ID ini tidak valid")
+        return value
+    
+    def validate_faculty_id(self, value):
+        if not Faculty.objects.filter(id=value).exists():
+            raise serializers.ValidationError("Faculty dengan ID ini tidak valid")
+        return value
+    
+    def validate_course_type_id(self, value):
+        if not CourseType.objects.filter(id=value).exists():
+            raise serializers.ValidationError("Course Type dengan ID ini tidak valid")
+        return value
+    
+    def create(self, validated_data):
+        faculty_id=validated_data.pop('faculty_id')
+        academic_program_id=validated_data.pop('academic_program_id')
+        course_type_id=validated_data.pop('course_type_id')
+        
+        faculty=get_object_or_404(Faculty, id=faculty_id)
+        academic_program=get_object_or_404(AcademicProgram, id=academic_program_id)
+        course_type=get_object_or_404(CourseType, id=course_type_id)
+        
+        validated_data['faculty']=faculty
+        validated_data['academic_program']=academic_program
+        validated_data['course_type']=course_type
+        return super().create(validated_data)
+    
     def update(self, instance, validated_data):
         academic_program=instance.academic_program
         if 'academic_program_id' in validated_data:
-            academic_program_id = validated_data.pop('academic_program_id')
-            if academic_program_id != instance.academic_program_id:
-                academic_program = AcademicProgram.objects.get(id=academic_program_id)
+            academic_program_id = validated_data.pop('academic_program_id', None)
+            if academic_program_id is not None and academic_program_id != instance.academic_program.id:
+                academic_program = get_object_or_404(AcademicProgram, id=academic_program_id)
                 validated_data['academic_program'] = academic_program
         
         faculty=instance.faculty
         if 'faculty_id' in validated_data:
-            faculty_id=validated_data.pop('faculty_id')
-            if faculty_id != instance.faculty_id:
-                faculty = Faculty.objects.get(id=faculty_id)
+            faculty_id=validated_data.pop('faculty_id', None)
+            if faculty_id is not None and faculty_id != instance.faculty.id:
+                faculty = get_object_or_404(Faculty, id=faculty_id)
                 validated_data['faculty'] = faculty
                 
-        course_type_id=validated_data.get('course_type_id')
+        course_type_id=validated_data.get('course_type_id', None)
         if course_type_id:
-            validated_data['course_type'] = CourseType.objects.get(id=course_type_id)
+            validated_data['course_type'] = get_object_or_404(CourseType, id=course_type_id)
         
         generate_new_code =  (
             'academic_program_id' in validated_data or
@@ -61,9 +91,8 @@ class CourseSerializer(serializers.ModelSerializer):
         
         if generate_new_code:
             is_specific=validated_data.get('is_specific', instance.is_specific)
-            
             if is_specific==1:
-                last_course=Course.objects.filter(faculty_id=faculty, is_specific=1, academic_program_id=academic_program).exclude(id=instance.id).order_by('-id').first()
+                last_course=Course.objects.filter(faculty_id=faculty.id, is_specific=1, academic_program_id=academic_program.id).exclude(id=instance.id).order_by('-id').first()
                 academic_program=academic_program.academic_program_code
                 academic_number=int(academic_program[-3:])
                 faculty=faculty.faculty_code
@@ -72,7 +101,7 @@ class CourseSerializer(serializers.ModelSerializer):
                 last_number=int(last_course.course_id[-3:])+1 if last_course else 1
                 validated_data['course_id']=f"{faculty}{academic_number:03d}1{year}1{last_number:03d}"
             elif is_specific==0:
-                last_course=Course.objects.filter(faculty_id=faculty,is_specific=0).exclude(id=instance.id).order_by('-id').first()
+                last_course=Course.objects.filter(faculty_id=faculty.id,is_specific=0).exclude(id=instance.id).order_by('-id').first()
                 
                 faculty=faculty.faculty_code
                 year=str(timezone.now().year)[-2:]
