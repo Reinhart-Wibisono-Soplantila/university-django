007f4ca60660b120ebb7f42f2ba069ec97535a31
reinhart <reinhartsoplantila@gmail.com>
2025-05-10 18:38:38 +0800

    app_building: refactor all serializer

--- app_building/serializers.py
+++ app_building/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Building, Room
 from app_common.serializers import FacultySerializer
 from app_common.models import Faculty
+from django.shortcuts import get_object_or_404
 
 # Gabungan untuk Read dan Write method
 class BuildingSerializer(serializers.ModelSerializer):
@@ -20,7 +21,20 @@ class BuildingSerializer(serializers.ModelSerializer):
         if not Faculty.objects.filter(id=value).exists():
             raise serializers.ValidationError("Faculty dengan ID ini tidak valid")
         return value
+    
+    def create(self, validated_data):
+        faculty_id=validated_data.pop("faculty_id", None)
+        if faculty_id is None:
+            raise serializers.ValidationError({"faculty_id": "Field ini wajib di isi."})
+        validated_data["faculty"]=get_object_or_404(Faculty, id=faculty_id)
+        return super().create(validated_data)
 
+    def update(self, instance, validated_data):
+        faculty_id=validated_data.pop("faculty_id", None)
+        if faculty_id is not None:
+            faculty=get_object_or_404(Faculty, id=faculty_id)
+            instance.faculty= faculty
+        return super().update(instance, validated_data)
     
     def to_representation(self, instance):
         rep=super().to_representation(instance)
@@ -34,7 +48,7 @@ class RoomSerializer(serializers.ModelSerializer):
     building_id=serializers.IntegerField(
         write_only=True,
         required=False,
-        allow_null=False,
+        allow_null=True,
     )
     building=BuildingSerializer(read_only=True)
     class Meta:
@@ -42,11 +56,23 @@ class RoomSerializer(serializers.ModelSerializer):
         fields='__all__'
         
     def validate_building_id(self, value):
-        if value is None:
-            return None
         if not Building.objects.filter(id=value).exists():
             raise serializers.ValidationError("Building dengan Id ini tidak valid")
         return value
+    
+    def create(self, validated_data):
+        building_id=validated_data.pop("building_id", None)
+        if building_id is None:
+            raise serializers.ValidationError({"building_id": "Field ini wajib diisi."})
+        validated_data["building"]=get_object_or_404(Building, id=building_id)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        building_id=validated_data.pop("building_id", None)
+        if building_id is not None:
+            building=get_object_or_404(Building, id=building_id)
+            instance.building= building
+        return super().update(instance, validated_data)
     
     def to_representation(self, instance):
         rep=super().to_representation(instance)
