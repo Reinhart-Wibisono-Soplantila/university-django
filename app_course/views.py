e7e5ae5ccb78770d71642409eb8388a60951e51c
reinhart <reinhartsoplantila@gmail.com>
2025-05-10 14:23:20 +0800

    app_course: add redis for coursetype method view

--- app_course/views.py
+++ app_course/views.py
@@ -3,24 +3,41 @@ from rest_framework.views import APIView
 from university.response import *
 from .models import Course, CourseType
 from .serializers import CourseSerializer, CourseTypeSerializer
+from django.core.cache import cache
+from django.db import transaction
 
 # Create your views here.
 class CourseTypeApiView(APIView):
+    CACHE_TIMEOUT=60*60
+    
+    def clear_cache_courseType(coursetype_id=None):
+        keys=["courseType_All"]
+        if coursetype_id:
+            keys.append(f"courseType_{coursetype_id}")
+        cache.delete_many(keys)
+        
     def get(self, request, coursetype_id=None):
-        if coursetype_id is not None:
-            coursetype_obj=get_object_or_404(CourseType, id=coursetype_id)
-            serializer=CourseTypeSerializer(coursetype_obj)
-        else:
-            coursetype_obj=CourseType.objects.all()
-            serializer=CourseTypeSerializer(coursetype_obj, many=True)
-        return success_response(serializer.data, message='success retrive data')
+        cache_key=f"courseType_{coursetype_id}" if coursetype_id else "courseType_All"
+        data=cache.get(cache_key)
+        if not data:
+            if coursetype_id is not None:
+                coursetype_obj=get_object_or_404(CourseType, id=coursetype_id)
+                serializer=CourseTypeSerializer(coursetype_obj)
+            else:
+                coursetype_obj=CourseType.objects.all()
+                serializer=CourseTypeSerializer(coursetype_obj, many=True)
+            data=serializer.data
+            cache.set(cache_key, data, timeout=self.CACHE_TIMEOUT)
+        return success_response(data, message='success retrive data')
     
     def post(self, request):
         serializer=CourseTypeSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         try:
-            serializer.save()
-            return success_response(serializer.data, message='success create data')
+            with transaction.atomic():
+                serializer.save()
+                self.clear_cache_courseType()
+                return success_response(serializer.data, message='success create data')
         except IntegrityError as e:
             error_clean = str(e).replace('\n', ' ').replace('"', '')
             raise ValidationError({error_clean})
@@ -30,8 +47,10 @@ class CourseTypeApiView(APIView):
         serializer=CourseTypeSerializer(coursetype_obj, data=request.data)
         serializer.is_valid(raise_exception=True)
         try:
-            serializer.save()
-            return success_response(serializer.data, message='success update data')
+            with transaction.atomic():
+                serializer.save()
+                self.clear_cache_courseType(coursetype_id)
+                return success_response(serializer.data, message='success update data')
         except IntegrityError as e:
             error_clean = str(e).replace('\n', ' ').replace('"', '')
             raise ValidationError({error_clean})
@@ -41,19 +60,24 @@ class CourseTypeApiView(APIView):
         serializer=CourseTypeSerializer(coursetype_obj, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         try:
-            serializer.save()
-            return success_response(serializer.data, message='success update data')
+            with transaction.atomic():
+                serializer.save()
+                self.clear_cache_courseType(coursetype_id)
+                return success_response(serializer.data, message='success update data')
         except IntegrityError as e:
             error_clean = str(e).replace('\n', ' ').replace('"', '')
             raise ValidationError({error_clean})
     
     def delete(self, request, coursetype_id):
         coursetype_obj=get_object_or_404(CourseType, id=coursetype_id)
-        coursetype_obj.delete()
-        return delete_reponse()
-    
-    def options(self, request, *args, **kwargs):
-        return super().options(request, *args, **kwargs)
+        try:
+            with transaction.atomic():
+                coursetype_obj.delete()
+                self.clear_cache_courseType(coursetype_id)
+                return delete_reponse()
+        except IntegrityError as e:
+            error_clean = str(e).replace('\n', ' ').replace('"', '')
+            raise ValidationError({error_clean})
 
 class CourseApiView(APIView):
     def get(self, request, course_id=None):
