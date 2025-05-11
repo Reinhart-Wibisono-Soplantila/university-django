433fc8a40e7f141b24ac9325438f918fb8a9c91d
reinhart <reinhartsoplantila@gmail.com>
2025-05-11 16:03:58 +0800

    app_course: refactor serializer

--- app_course/serializers.py
+++ app_course/serializers.py
@@ -11,85 +11,50 @@ class CourseTypeSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     academic_program=AcademicProgramSerializer(read_only=True)
-    academic_program_id=serializers.IntegerField(
+    academic_program_id=serializers.PrimaryKeyRelatedField(
+        queryset=AcademicProgram.objects.all(),
         write_only=True,
         required=True,
-        allow_null=False
-    )
+        source='academic_program',
+        error_messages={
+            'does_not_exist': 'Academic Program dengan ID {pk_value} tidak ditemukan',
+            'incorrect_type': 'Academic Program ID harus berupa angka',
+            'required': 'Field Academic Program harus diisi!'})
     
     faculty=FacultySerializer(read_only=True)
-    faculty_id=serializers.IntegerField(
+    faculty_id=serializers.PrimaryKeyRelatedField(
+        queryset=Faculty.objects.all(),
         write_only=True,
         required=True,
-        allow_null=False
-    )
+        source='faculty',
+        error_messages={
+            'does_not_exist': 'Faculty dengan ID {pk_value} tidak ditemukan',
+            'incorrect_type': 'Faculty ID harus berupa angka',
+            'required': 'Field Faculty harus diisi!'})
     
     course_type=CourseTypeSerializer(read_only=True)
-    course_type_id=serializers.IntegerField(
+    course_type_id=serializers.PrimaryKeyRelatedField(
+        queryset=CourseType.objects.all(),
         write_only=True,
         required=True,
-        allow_null=False
-    )
+        source='course_type',
+        error_messages={
+            'does_not_exist': 'Course Type dengan ID {pk_value} tidak ditemukan',
+            'incorrect_type': 'Course Type ID harus berupa angka',
+            'required': 'Field Course Type harus diisi!'})
     
     class Meta:
         model=Course
         fields='__all__'
         read_only_fields=['course_id']
     
-    def validate_academic_program_id(self, value):
-        if not AcademicProgram.objects.filter(id=value).exists():
-            raise serializers.ValidationError("Academic Program dengan ID ini tidak valid")
-        return value
-    
-    def validate_faculty_id(self, value):
-        if not Faculty.objects.filter(id=value).exists():
-            raise serializers.ValidationError("Faculty dengan ID ini tidak valid")
-        return value
-    
-    def validate_course_type_id(self, value):
-        if not CourseType.objects.filter(id=value).exists():
-            raise serializers.ValidationError("Course Type dengan ID ini tidak valid")
-        return value
-    
-    def create(self, validated_data):
-        faculty_id=validated_data.pop('faculty_id')
-        academic_program_id=validated_data.pop('academic_program_id')
-        course_type_id=validated_data.pop('course_type_id')
-        
-        faculty=get_object_or_404(Faculty, id=faculty_id)
-        academic_program=get_object_or_404(AcademicProgram, id=academic_program_id)
-        course_type=get_object_or_404(CourseType, id=course_type_id)
-        
-        validated_data['faculty']=faculty
-        validated_data['academic_program']=academic_program
-        validated_data['course_type']=course_type
-        return super().create(validated_data)
-    
     def update(self, instance, validated_data):
-        academic_program=instance.academic_program
-        if 'academic_program_id' in validated_data:
-            academic_program_id = validated_data.pop('academic_program_id', None)
-            if academic_program_id is not None and academic_program_id != instance.academic_program.id:
-                academic_program = get_object_or_404(AcademicProgram, id=academic_program_id)
-                validated_data['academic_program'] = academic_program
-        
-        faculty=instance.faculty
-        if 'faculty_id' in validated_data:
-            faculty_id=validated_data.pop('faculty_id', None)
-            if faculty_id is not None and faculty_id != instance.faculty.id:
-                faculty = get_object_or_404(Faculty, id=faculty_id)
-                validated_data['faculty'] = faculty
-                
-        course_type_id=validated_data.get('course_type_id', None)
-        if course_type_id:
-            validated_data['course_type'] = get_object_or_404(CourseType, id=course_type_id)
-        
-        generate_new_code =  (
-            'academic_program_id' in validated_data or
-            'faculty_id' in validated_data or
-            'is_specific' in validated_data)
-        
-        if generate_new_code:
+        academic_program_changed = 'academic_program' in validated_data and validated_data['academic_program'] != instance.academic_program
+        faculty_changed = 'faculty' in validated_data and validated_data['faculty'] != instance.faculty
+
+        if academic_program_changed or faculty_changed:
+            faculty=validated_data.get('faculty', instance.faculty)
+            academic_program=validated_data.get('academic_program', instance.academic_program)
             is_specific=validated_data.get('is_specific', instance.is_specific)
             if is_specific==1:
                 last_course=Course.objects.filter(faculty_id=faculty.id, is_specific=1, academic_program_id=academic_program.id).exclude(id=instance.id).order_by('-id').first()
@@ -102,26 +67,16 @@ class CourseSerializer(serializers.ModelSerializer):
                 validated_data['course_id']=f"{faculty}{academic_number:03d}1{year}1{last_number:03d}"
             elif is_specific==0:
                 last_course=Course.objects.filter(faculty_id=faculty.id,is_specific=0).exclude(id=instance.id).order_by('-id').first()
-                
                 faculty=faculty.faculty_code
                 year=str(timezone.now().year)[-2:]
-                
                 last_number=int(last_course.course_id[-3:])+1 if last_course else 1
                 validated_data['course_id']=f"{faculty}1{year}1{last_number:03d}"
         return super().update(instance, validated_data)
     
     def to_representation(self, instance):
         rep=super().to_representation(instance)
-        if "faculty" in rep and rep['faculty'] is not None:
-            data=rep["faculty"]
-            data.pop("created_at", None)
-            data.pop("updated_at", None)
-        if "academic_program" in rep and rep["academic_program"] is not None:
-            data=rep["academic_program"]
-            data.pop("created_at", None)
-            data.pop("updated_at", None)
-        if "course_type" in rep and rep["course_type"] is not None:
-            data=rep["course_type"]
-            data.pop("created_at", None)
-            data.pop("updated_at", None)
+        for field in ["faculty", "academic_program", "course_type"]:
+            if field in rep and rep[field] is not None:
+                rep[field].pop("created_at", None)
+                rep[field].pop("updated_at", None)
         return rep
